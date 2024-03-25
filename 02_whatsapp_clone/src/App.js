c4ca7223b0827d59ebfdff103139c603304014e5
adwitajindal <adwita.jindal.42@gmail.com>
2024-03-25 22:22:30 +0530

    firebase connected

---
+++ 02_whatsapp_clone/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>lets build a mern whatsapp clone</h1>
+    </div>
+  );
+}
+
+export default App;
